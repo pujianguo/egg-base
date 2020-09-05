da80d56a97c1717f425cf7a49d1ecbedc12b9a36
raul <18919015125@163.com>
2020-09-05 22:13:53 +0800

    修改 loginResponse 结构

--- app/contract/response/access.js
+++ app/contract/response/access.js
@@ -2,10 +2,7 @@
 
 module.exports = {
   loginResponse: {
-    _id: { type: 'string', description: 'id 唯一键' },
-    phone: { type: 'string', description: '手机' },
-    realName: { type: 'string', description: '用户姓名' },
-    avatar: { type: 'string', description: '头像' },
     token: { type: 'string', description: 'Token' },
+    user: { type: 'user', description: '用户信息' },
   },
 };
--- app/contract/response/user.js
+++ app/contract/response/user.js
@@ -20,11 +20,11 @@ module.exports = {
     updatedAt: { type: 'string', description: '修改时间' },
   },
 
-  loginResponse: {
-    _id: { type: 'string', description: 'id 唯一键' },
-    phone: { type: 'string', description: '手机' },
-    realName: { type: 'string', description: '用户姓名' },
-    avatar: { type: 'string', description: '头像' },
-    token: { type: 'string', description: 'Token' },
-  },
+  // loginResponse: {
+  //   _id: { type: 'string', description: 'id 唯一键' },
+  //   phone: { type: 'string', description: '手机' },
+  //   realName: { type: 'string', description: '用户姓名' },
+  //   avatar: { type: 'string', description: '头像' },
+  //   token: { type: 'string', description: 'Token' },
+  // },
 };
