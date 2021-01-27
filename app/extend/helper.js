f10ef6857e934fdb270253efa8e0c1d513807d95
raul <18919015125@163.com>
2021-01-27 16:09:16 +0800

    add: upload

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -3,6 +3,13 @@
 const moment = require('moment');
 const crypto = require('crypto');
 
+/**
+ * 函数中有以下变量：
+ * this 是 helper 对象，在其中可以调用其他 helper 方法
+ * this.ctx => context 对象
+ * this.app => application 对象
+ */
+
 module.exports = {
   formatMonth: t => (t ? moment(t).format('YYYY-MM') : ''),
   formatDate: t => (t ? moment(t).format('YYYY-MM-DD') : ''),
